e65fd56c8087965ce7eb9360c857f0c123064681
Jake Wehder <jw@email.comjake>
2023-01-10 15:58:46 -0600

    adding cards to finish barebones frontend

--- salestracker-app-frontend/src/components/App.js
+++ salestracker-app-frontend/src/components/App.js
@@ -3,6 +3,9 @@ import OrderForm from './OrderForm';
 import Button from 'react-bootstrap/Button';
 import OrderTable from './OrderTable';
 import CloseButton from 'react-bootstrap/CloseButton';
+import InfoCard from "./InfoCard";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
 function App() {
   const [formIsActive, setFormActive] = useState(false)
@@ -18,6 +21,20 @@ function App() {
         
         {formIsActive ? <><hr></hr> <OrderForm /></> : ""}
         <hr></hr>
+      </div>
+      <div>
+        <Row xs={1} md={2} className="g-4">
+          <Col>
+            <InfoCard />
+          </Col>
+          <Col>
+            <InfoCard />
+          </Col>
+          <Col>
+            <InfoCard />
+          </Col>
+          <hr></hr>
+        </Row>
       </div>
       <div>
         <OrderTable />
